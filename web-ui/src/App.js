600fa9bb107ccd87e227cb10284938fb031f510e
Viraat <viraat.laldas@gmail.com>
2024-03-23 22:04:23 -0400

    adding links and google tag

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import './App.css';
-import ImageUpload from './components/ImageUpload';
+import React from "react";
+import "./App.css";
+import ImageUpload from "./components/ImageUpload";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="description">
+          Welcome to the Colony Counter application. Upload images of petri
+          dishes to automatically count colonies.
+        </p>
         <h1>Colony Counter</h1>
         <main>
-        <ImageUpload />
-      </main>
+          <ImageUpload />
+        </main>
+        <p className="description">
+          <a
+            href="https://github.com/viraatdas/ColonyCounter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/viraatdas/ColonyCounter
+          </a>
+        </p>
       </header>
-     
     </div>
   );
 }
