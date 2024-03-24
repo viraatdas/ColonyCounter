a8dbc6b0ec4d0ae2dd6dff575046d50e86ce6505
Viraat <viraat.laldas@gmail.com>
2024-03-23 22:59:33 -0400

    removing useeffect

--- web-ui/src/components/ImageUpload.js
+++ web-ui/src/components/ImageUpload.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { BeforeAfter } from "react-simple-before-after";
 import sampleImage from "../assets/sample_1.png";
 
